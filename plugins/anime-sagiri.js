af03f0fdc3953c1390cf8add787ce8257b6653de
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-26 20:29:04 +0700

    Update anime-sagiri.js

--- plugins/anime-sagiri.js
+++ plugins/anime-sagiri.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-  conn.sendFile(m.chat, 'https://api.lolhuman.xyz/api/random/sagiri?apikey=Papah-Chan', '', '', m)
+  conn.sendFile(m.chat, 'https://api.lolhuman.xyz/api/random/sagiri?apikey=APIKEYKU', '', '', m)
 }
 handler.help = ['sagiri']
 handler.tags = ['anime']
